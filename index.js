51cb07cab7ce02b8b461f90f07102d87311a65fb
Mansimran Singh <me@mansimransingh.com>
2015-01-24 13:09:24 +0000

    body parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var app = express()
+var app = express();
+var bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({extended: false}));
     app.get('/', function (req, res) {
         console.log(req.body);
         res.send('Hello world!');
